4eb674f67a9070ae37fd990414b05617a1f640c7
oty-git <78175358+oty-git@users.noreply.github.com>
2025-07-19 12:55:49 +0300

    feat: deploy to gh pages

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -3,20 +3,25 @@ import MainLayout from '../layouts/MainLayout';
 import HomePage from '../pages/HomePage';
 import TrialsPage from '../pages/TrialsPage';
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: '/',
+      element: <MainLayout />,
+      children: [
+        {
+          index: true,
+          element: <HomePage />,
+        },
+        {
+          path: 'trials',
+          element: <TrialsPage />,
+        },
+        // More routes will be added here later
+      ],
+    },
+  ],
   {
-    path: '/',
-    element: <MainLayout />,
-    children: [
-      {
-        index: true,
-        element: <HomePage />,
-      },
-      {
-        path: 'trials',
-        element: <TrialsPage />,
-      },
-      // More routes will be added here later
-    ],
-  },
-]);
+    basename: '/agent-frontend',
+  }
+);
