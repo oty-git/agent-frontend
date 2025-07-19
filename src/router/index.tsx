7334c8fa5a88b0b545f6174e8640f7623dbc7275
oty-git <78175358+oty-git@users.noreply.github.com>
2025-07-19 10:09:21 +0300

    feat: create TrialsPage

--- src/router/index.tsx
+++ src/router/index.tsx
@@ -1,6 +1,7 @@
 import { createBrowserRouter } from 'react-router-dom';
 import MainLayout from '../layouts/MainLayout';
 import HomePage from '../pages/HomePage';
+import TrialsPage from '../pages/TrialsPage';
 
 export const router = createBrowserRouter([
   {
@@ -10,6 +11,10 @@ export const router = createBrowserRouter([
       {
         index: true,
         element: <HomePage />,
+      },
+      {
+        path: 'trials',
+        element: <TrialsPage />,
       },
       // More routes will be added here later
     ],
